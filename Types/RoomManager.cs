2d35918d1c9e54731b78e94b572c3bcec5a51bfc
SirBrahms <95311570+SirBrahms@users.noreply.github.com>
2024-05-09 21:35:07 +0200

    more work on RoomManager

--- Types/RoomManager.cs
+++ Types/RoomManager.cs
@@ -3,7 +3,7 @@ using Game.Items;
 
 namespace Game.Types;
 
-static class RoomMAnager
+static class RoomManager
 {
     #pragma warning disable // Init must be called anyway
     public static Room CurrentRoom { get; set; }
@@ -19,6 +19,7 @@ static class RoomMAnager
         return new Room(() => {}, () => {}, () => {}) { Enemies = GetRandomEnemies() };
     }
 
+    // move to enemy manager
     private static List<IEnemy> GetRandomEnemies()
     {
         List<IEnemy> EnemiesRet = new List<IEnemy>();
@@ -26,7 +27,7 @@ static class RoomMAnager
         for (int i = 0; i > r.Next(1, 5); i++)
         {
             var Enemies = EnemyManager.GetAllEnemies();
-            var CurrentEnemyType = Enemies[r.Next()];
+            var CurrentEnemyType = Enemies[r.Next(Enemies.Count - 1)];
 
             var EnemyInstance = (IEnemy?)Activator.CreateInstance(CurrentEnemyType) ?? throw new Exception("Somehow got Null: GetRandomEnemy");
             EnemiesRet.Add(EnemyInstance);
